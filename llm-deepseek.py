23bf26ab3a09ab01bd655b5ee8295ee180583a99
Екатерина Соколова <ksokolova2468@gmail.com>
2026-07-02 11:28:40 +0300

    созданы docker_compose и llm-deepseek

---
+++ llm-deepseek.py
@@ -0,0 +1,20 @@
+import os
+from openai import OpenAI
+
+os.environ['DEEPSEEK_API_KEY'] = 'sk-beb0645699024d40abc08597b1ddb64d'  # <-- вот так!
+
+client = OpenAI(
+    api_key=os.environ.get('DEEPSEEK_API_KEY'),
+    base_url="https://api.deepseek.com"
+)
+
+response = client.chat.completions.create(
+    model="deepseek-v4-pro",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
